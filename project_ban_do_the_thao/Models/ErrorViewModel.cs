57084853b09b07b81f9a199ba0bdd29bd7660f80
Khanh <mqkhanh04@gmail.com>
2025-04-13 11:46:11 +0700

    demo

---
+++ project_ban_do_the_thao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace project_ban_do_the_thao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
